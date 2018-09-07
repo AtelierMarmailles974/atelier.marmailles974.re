9935b5290edd5d1e27634f134b0be37eb42e33f5
Gregory Bouteiller <gregory.bouteiller@niama.re>
2018-09-07 14:57:40 +0400

    upgrade

--- pages/mentions-legales.vue
+++ pages/mentions-legales.vue
@@ -2,14 +2,18 @@
 include ../styles/mixins
 div
   v-hero(:hero="hero")
+  v-section(ct="greyVVL"): ul: li(v-for="a of articles")
+    h4 {{ a.title }}
+    div(v-html="a.content")
 </template>
 
 
 <script lang="ts">
 import { Component, FelaMixin, mixins } from 'nuxt-fela';
 
+import articles from '~/content/articles';
 import { hero } from '~/content/pages/legale-notice.json';
-import { Hero } from '~/definitions';
+import { Article, Hero } from '~/definitions';
 
 @Component({ head: { title: 'Mentions légales - ATELIER MARMAILLES 974' } } as any)
 export default class LegaleNoticePage extends mixins(FelaMixin) {
@@ -17,6 +21,7 @@ export default class LegaleNoticePage extends mixins(FelaMixin) {
   // PROPS
   // =================================================================================================================================
 
+  articles: Article[] = articles;
   hero: Hero = hero;
 }
 </script>
